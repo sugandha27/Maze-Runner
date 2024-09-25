54697c8589aac9b26a31d4f65871154615323428
Sugandha Bhandari <sugandhab@Sugandhas-MacBook-Air-2.local>
2024-09-25 15:11:46 -0700

    adjacency list representation added

---
+++ adjacencyList.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class adjacencyList {
+    private int vertices;
+    private LinkedList<Integer>[] adjList;
+
+    public adjacencyList(int vertices){
+        this.vertices = vertices;
+        
+    }
+    
+}
