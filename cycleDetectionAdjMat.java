b0cec58ef059b3adc2fc12a1325bb650787bbcb4
Sugandha Bhandari <sugandhab@Sugandhas-MacBook-Air-2.local>
2024-09-25 13:20:23 -0700

    dfs recursion and has cycles added

--- cycleDetectionAdjMat.java
+++ cycleDetectionAdjMat.java
@@ -1,16 +1,52 @@
 public class cycleDetectionAdjMat{
-    // cycle detection for a graph represented as adj matrix
-    private int vertices;
+    // cycle detection for a graph represented as adj matrix using DFS
+    private int vertices; //total number of vertices
     private int[][] adjMatrix;
+    private boolean[] visited;
 
     //constructor that takes input as vertex
     public cycleDetectionAdjMat(int vertices){
         this.vertices = vertices;
         adjMatrix = new int[vertices][vertices];
+        visited = new boolean[vertices];
     }
 
     //add edge weight to adjacency matrix
     public void addEdge(int src, int dest, int weight){
         
+        adjMatrix[src][dest] = weight;
+        adjMatrix[dest][src] = weight; // for undirected graph 
+    }
+
+    // helper function for recursive dfs 
+    private boolean dfs(int vertex, int parent){
+        //mark the called vertex as visited
+        visited[vertex] = true;
+
+        for (int i = 0; i < vertices; i++ ){
+            if(adjMatrix[vertex][i] != 0){
+                if(!visited[i]){
+                    if(dfs(i,vertex))
+                    return true;
+                }
+                else if(i!=parent)
+                return true;  //cycle detected
+            }
+        }
+        return false;
+    }
+
+    //check cycles for entire graph
+    // Checks each vertex of the graph to ensure all components are covered.
+    public boolean hasCycle(){
+        for(int i = 0; i< vertices; i++){
+            if(!visited[i]){
+                if(dfs(i, -1)){
+                    return true;
+                }
+            }
+            // For each unvisited vertex i, dfs(i, -1) is called, where -1 indicates no parent for the initial call.
+        }
+        return false;
     }
 }
