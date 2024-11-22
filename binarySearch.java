a7e063f298c4f89601370f9f14f5878d577843fc
sugandha27 <sugandhabhandari2011@gmail.com>
2024-11-21 23:11:12 -0800

    binary search code added

--- binarySearch.java
+++ binarySearch.java
@@ -1,8 +1,16 @@
 public class binarySearch {
-    // public int bSearch(int[] nums){
-    //     int low = 0;
-    //     int high = nums.length-1;
-
-    //     return mid;
-    // }
+    public int search(int[] nums, int target) {
+        int high = nums.length-1;
+        int low = 0;
+        while(low<high){
+            if(nums[(high+low)/2] == target)
+            return (high+low)/2;
+            else if(nums[(high+low)/2] < target)
+            low = (high+low)/2 +1;
+            else high = (high+low)/2 ;
+        }
+        if(low == high && nums[low]==target)
+        return low;
+        return -1;
+    }
 }
